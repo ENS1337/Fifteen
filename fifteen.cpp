4c8393eaf69a6b7a60e66a47f79d8de7f6c3fb20
ENS1337 <sokol0198@yandex.ru>
2017-05-18 23:02:56 +0700

    Fifteen coursework

---
+++ fifteen.cpp
@@ -0,0 +1,354 @@
+#include "fifteen.h"
+#include "ui_fifteen.h"
+#include <QtCore>
+#include <QLabel>
+#include <QtGui>
+#include <QVector>
+#include <QMessageBox>
+#include <QElapsedTimer>
+#include <QSettings>
+#include <QColor>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+	ui->setupUi(this);
+
+	arr = NULL;
+	moves_number = 0;
+    collected_tiles = 0;
+    ui->label->setText("Плиток собрано: " + QString::number(collected_tiles));
+
+
+	Generate_Tiles(false);
+	this->setFocus();
+}
+
+void MainWindow::Generate_Tiles(bool random) // функция для рандомного создания плиток
+{
+    QSettings set;
+
+	if (!random)
+	{
+		timer_started = false;
+		can_move = false;
+		ui->label_Time->setText("Время: 00:00");
+		ui->label_Moves->setText("Ходов: 0");
+		ui->pushButton->setText("Начать игру");
+	}
+	else
+	{
+		startTimer(0);
+		mStartTime = QDateTime::currentDateTime();
+		timer_started = true;
+		can_move = true;
+	}
+
+	int z = 0;
+	if (arr != NULL) //если массив уже инициализирован
+	{
+		for (int i = 0; i < 4; i++) // проходим по матрице плиток (двумерному массиву)
+		{
+			for (int j = 0; j < 4; j++,z++)
+			{
+				if (z >= 16) break;
+
+				if (arr[i][j] != NULL)
+				{
+					ui->gridLayout->removeWidget(arr[i][j]); //удаляем плитку с экрана
+					disconnect(arr[i][j], SIGNAL(pressed()), this, SLOT(processButton()));
+
+					delete arr[i][j]; // удаляем плитку из памяти
+					arr[i][j] = NULL; // для безопасности присваиваем NULL
+				}
+			}
+		}
+		delete arr;
+	}
+
+	z = 0;
+	arr = new QPushButton**[4];// создаем массив заново
+
+	srand(time(NULL));
+
+	std::vector<int> vec; // создаем вектор данных, он нужен для того, чтобы можно было перемешивать номера плиток
+	for (int i = 0; i < 15; i++)
+	{
+		vec.push_back(i); // забиваем в него числа от 1 до 15
+	}
+
+	std::random_shuffle(vec.begin(),vec.end(),my_rand); // собственно, перемешиваем числа
+
+	for (int i = 0; i < 4; i++)
+	{
+		arr[i] = new QPushButton*[4]; //создаем строку в двумерном массиве
+
+		for (int j = 0; j < 4; j++,z++)
+		{
+			arr[i][j] = NULL;
+			if (z >= 15) break;
+
+			QPushButton *label = new QPushButton(this); // создаем плитку
+
+            pal.setColor(QPalette::ButtonText, Qt::white);
+			if (random == true)
+			{
+                label->setText(QString::number(vec[z] + 1)); // текст, который будет на ней отображаться - число из вектора
+			}
+			else
+			{
+				label->setText(QString::number(z+1)); // текст, который будет на ней отображаться - число
+			}
+			label->setFont(QFont(set.value("font-family").toString(), set.value("font-size").toInt(), QFont::Bold)); // делаем побольше шрифт
+			label->setPalette(pal);
+            label->setMinimumSize(80,80);
+            label->setStyleSheet("background-color: black;"); // делаем плитки черными
+
+			connect(label, SIGNAL(pressed()), this, SLOT(processButton()));
+
+			arr[i][j] = label; // записываем кнопку в массив
+		}
+	}
+    z = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++, z++)
+		{
+			if (z >= 15) break;
+			ui->gridLayout->addWidget(arr[i][j],i,j); // добавляем плитку на экран
+		}
+	}
+}
+
+MainWindow::~MainWindow()
+{
+	delete ui;
+}
+
+bool MainWindow::has_won() // функция, которая проверяет, расставлены ли плитки по порядку(выиграл ли игрок)
+{
+    int z = 0;
+    bool is_winner = false;
+    collected_tiles=0;
+   for (int i = 0; i < 4; i++) //Подсчет Кол-ва собранных плиток по порядку
+    {
+        for (int j = 0; j < 4; j++, z++)
+        {
+            if (z >= 15) break;
+            if (arr[i][j] != NULL)
+            {
+                if (arr[i][j]->text() == QString::number(z+1))
+                {
+                    collected_tiles++;
+                }
+            }
+        }
+    }
+   ui->label->setText("Плиток собрано: " + QString::number(collected_tiles));
+   z=0;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++, z++)
+		{
+			if (z >= 15) break;
+			if (arr[i][j] != NULL)
+			{
+				if (arr[i][j]->text() == QString::number(z+1))
+                    is_winner = true;
+				else
+				{
+                    is_winner = false;
+                    return is_winner;
+				}
+			}
+            else return false;
+		}
+    }
+
+    z=0;
+    if (is_winner == true) // При победе игрока, плитки перекрашиваются в зеленый
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++, z++)
+            {
+                if (z >= 15) break;
+                if (arr[i][j] != NULL)
+                {
+                    arr[i][j]->setStyleSheet("background-color: green;");
+                }
+            }
+        }
+    }
+	return is_winner;
+}
+
+void MainWindow::MoveCell(QPushButton *w, int arr_i, int arr_j) // передвижение плитки, если есть свободная клетка от нее, или не передвигать
+{
+	// проверяем, свободно ли снизу
+	if (arr_i < 3)
+	{
+		if (arr[arr_i+1][arr_j] == NULL)
+		{
+			ui->gridLayout->addWidget(w,arr_i+1, arr_j);
+
+			arr[arr_i+1][arr_j] = w;
+			arr[arr_i][arr_j] = NULL;
+			if (has_won())
+				qDebug() << "Winner!";
+			moves_number++;
+			return;
+		}
+	}
+
+	// проверяем, свободно ли сверху
+	if (arr_i > 0)
+	{
+		if (arr[arr_i-1][arr_j] == NULL)
+		{
+			ui->gridLayout->addWidget(w,arr_i-1, arr_j);
+			arr[arr_i][arr_j] = NULL;
+			arr[arr_i-1][arr_j] = w;
+			if (has_won())
+				qDebug() << "Winner!";
+			moves_number++;
+			return;
+		}
+	}
+
+	// проверяем, свободно ли слева
+	if (arr_j > 0)
+	{
+		if (arr[arr_i][arr_j-1] == NULL)
+		{
+			ui->gridLayout->addWidget(w,arr_i, arr_j-1);
+			arr[arr_i][arr_j] = NULL;
+			arr[arr_i][arr_j-1] = w;
+			if (has_won())
+				qDebug() << "Winner!";
+			moves_number++;
+			return;
+		}
+    }
+
+    // проверяем, свободно ли справа
+	if (arr_j < 3)
+	{
+		if (arr[arr_i][arr_j+1] == NULL)
+		{
+			ui->gridLayout->addWidget(w,arr_i, arr_j+1);
+			arr[arr_i][arr_j] = NULL;
+			arr[arr_i][arr_j+1] = w;
+			if (has_won())
+				qDebug() << "Winner!";
+			moves_number++;
+			return;
+		}
+	}
+
+}
+
+void MainWindow::processButton() // по нажатию плитки выполняется эта функция
+{
+	if (can_move)
+	{
+		QPushButton *obj = qobject_cast<QPushButton*>(sender());
+		qDebug() << obj->text();
+
+        int i = 0, j = 0;  // ищем индекс (на каком столбце и строке стоит плитка в матрице)
+
+		for (i = 0; i < 4; i++)
+		{
+			for (j = 0; j < 4; j++)
+			{
+				if (arr[i][j] == obj) // нашлась плитка
+				{
+					MoveCell(obj,i,j); // двигаем плитки
+					ui->label_Moves->setText("Ходов: " + QString::number(moves_number));
+					if(has_won()) // если плитки расставлены по порядку
+					{
+						timer_started = false;
+						can_move = false;
+
+						QString best_score;
+						QSettings set("Neridia, Inc.", "Fifteen"); // открываем настройки
+
+						QMessageBox msg;
+						msg.setText("Вы выиграли!"); // выводим сообщение о выигрыше
+						msg.setStandardButtons(QMessageBox::Ok);
+
+						if (set.contains("best_score"))
+						{
+							qDebug() << "Best_score: " << set.value("best_score").toInt() << " moves_number: " << moves_number;
+							if (set.value("best_score").toInt() > moves_number)
+							{
+								msg.setInformativeText("Вы установили новый рекорд! Ваше число ходов: " + QString::number(moves_number) + "\n");
+								msg.setInformativeText(msg.informativeText() + "Предыдущий рекорд: " + set.value("best_score").toString());
+								set.setValue("best_score", moves_number);
+							}
+							else
+							{
+								msg.setInformativeText("Число ходов: " + QString::number(moves_number));
+							}
+						}
+						else
+						{
+							msg.setInformativeText("Число ходов: " + QString::number(moves_number));
+							set.setValue("best_score", moves_number);
+						}
+						msg.exec();
+					}
+					return;
+				}
+			}
+		}
+	}
+}
+
+int my_rand(int i)
+{
+	return rand() % i;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+	ui->label_Moves->setText("Ходов: 0");
+	moves_number = 0;
+	Generate_Tiles(true);
+	ui->pushButton->setText("Перезапустить");
+}
+
+void MainWindow::timerEvent(QTimerEvent *)
+{
+	if(timer_started)
+	{
+		qint64 ms = mStartTime.msecsTo(QDateTime::currentDateTime());
+		int h = ms / 1000 / 60 / 60;
+		int m = (ms / 1000 / 60) - (h * 60);
+		int s = (ms / 1000) - (m * 60);
+		if (m < 10)
+		{
+			all_time = '0' + QString::number(m) + ':';
+		}
+		else {
+			all_time = QString::number(m) + ':';
+		}
+
+		if (s < 10)
+		{
+			all_time += '0' + QString::number(s);
+		}
+		else {
+			all_time += QString::number(s);
+		}
+
+		ui->label_Time->setText("Время: " + all_time);
+	}
+}
+
+void MainWindow::resizeEvent(QResizeEvent *event)
+{
+	QMainWindow::resizeEvent(event); //for debugging
+	qDebug() << this->width() << " " << this->height();
+}
---
+++ fifteen.h
@@ -0,0 +1,57 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtGui>
+#include <QLabel>
+#include <QPushButton>
+#include <QDialog>
+#include <QElapsedTimer>
+#include <QSettings>
+
+namespace Ui {
+	class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
+	void MoveCell(QPushButton *w, int arr_i, int arr_j);
+	void timerEvent(QTimerEvent *);
+
+	QVector <QLabel*> buttons_vec; // создание вектора, хранящего надписи, их мы будем двигать по таблице
+
+	void resizeEvent(QResizeEvent *event);
+
+private:
+	Ui::MainWindow *ui;
+	QPushButton ***arr;
+	QLabel *emptylabel;
+	QDialog dialog;
+	int prev_num;
+	bool has_won();
+	int moves_number; // переменная - счетчик числа ходов
+	QLabel *moves_info; // строка, которая будет записывать количество ходов
+	QLabel *time_label; // строка, которая будет вести счет
+	QPalette pal;//палитра для того, чтобы окрасить текст в белый цвет
+	bool timer_started;
+	QElapsedTimer *timer; //таймер
+	QDateTime mStartTime; // время начала игры
+	QString all_time;
+	bool can_move; // может ли игрок двигать плитки
+    int collected_tiles;
+
+public slots:
+	void processButton();
+private slots:
+	void on_pushButton_clicked();
+	void Generate_Tiles(bool random = false);
+};
+
+int my_rand(int i);
+
+#endif // MAINWINDOW_H
